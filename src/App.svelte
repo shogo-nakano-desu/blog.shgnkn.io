b0a34c0e7ff3fcbab3cc45791c5e7806118d87fc
shogo-nakano-desu <61229807+shogo-nakano-desu@users.noreply.github.com>
2022-01-01 22:15:40 +0900

    delete page. use supper

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
   import Header from './components/Header.svelte';
-  import Body from './components/Body.svelte';
 </script>
 
 <main>
   <div class="containar">
     <Header />
-
-    <!-- <Body /> -->
   </div>
 </main>
 
 <style>
+  #containar {
+    width: 100%;
+    height: 100%;
+  }
 </style>
