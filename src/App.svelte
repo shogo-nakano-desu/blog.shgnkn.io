a62d2086ec692007cb23fca2aa6114290be5b7fe
shogo-nakano-desu <61229807+shogo-nakano-desu@users.noreply.github.com>
2022-01-01 20:32:17 +0900

    now I can use .md file as svelte component

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
   import Header from './components/Header.svelte';
+  import Body from './components/Body.svelte';
 </script>
 
 <main>
   <div class="containar">
     <Header />
+    <Body />
   </div>
 </main>
 
--- src/components/body.svelte
+++ src/components/body.svelte
@@ -1,27 +1,10 @@
 <script>
+  import Index from '../contents/202201-start-blog-with-svelte/index.md';
 </script>
 
-<body>
-  <div>
-    <h2><a href="https://WIP/{title}">[TODO]{title}</a></h2>
-  </div>
-</body>
+<div>
+  <Index />
+</div>
 
 <style>
-  header {
-    background-color: rgb(103, 103, 120);
-    color: rgb(255, 254, 252);
-  }
-  div {
-    margin: auto;
-    max-width: 960px;
-    padding: 1.45rem 1.0875rem;
-  }
-  a {
-    color: rgb(255, 254, 252);
-    text-decoration: none;
-  }
-  a:visited {
-    color: inherit;
-  }
 </style>
