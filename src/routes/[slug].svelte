dd80ce7d47daece7b0d20ef7752709c1a2a44493
shogo-nakano-desu <61229807+shogo-nakano-desu@users.noreply.github.com>
2022-01-03 23:57:09 +0900

    now I can import component. I will change it to dynamic import

--- src/routes/[slug].svelte
+++ src/routes/[slug].svelte
@@ -10,19 +10,17 @@
 </script>
 
 <script>
-  let pathname
+  // let pathname
   // import Index from '../contents{pathnaem}/Index.md';
   // import { metadata } from `../contents${pathnaem}/Index.md`;
-  export const load = async() => {
-    let Index = await import(`../contents${pathname}/Index.md`)
-    let metadata = await import(`../contents${pathname}/Index.md`)
-    return {
-      Index: Index,
-      metadata: metadata
-    }
-  }
-  let promise = load()
-
+  import { onMount } from 'svelte';
+    let Index
+    let metadata
+  onMount(async () => {
+    Index = (await import('../contents/202201-start-blog-with-svelte/Index.md')).default
+    console.log(Index)
+    await import('../contents/202201-start-blog-with-svelte/Index.md')
+    })
 
 </script>
 
@@ -37,15 +35,8 @@
   {/await}
 </svelte:head> -->
 
-<article>
-  {#await promise}
-  <h1>WIP</h1>
-  {:then result}
-  <!-- <h1>{result.metadata.title}</h1> -->
-  {result.Index}
-  {:catch error}
-  <h1>faile to get title</h1>
-  {/await}
-  <!-- here will be contents -->
 
-</article>
+
+<svelte:component this={Index}>
+	<p>some slotted content</p>
+</svelte:component>
