e3e8a374392437afd367296bd94296c9b42a88ea
shogo-nakano-desu <61229807+shogo-nakano-desu@users.noreply.github.com>
2022-01-07 23:59:39 +0900

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "blog-shgnkn-gatsby",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "",
+      },
+    },
+  ],
+};
