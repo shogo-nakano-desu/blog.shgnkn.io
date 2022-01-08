a7fe7e717f9c76cbb79828a98cdac9df48eca882
shogo-nakano-desu <61229807+shogo-nakano-desu@users.noreply.github.com>
2022-01-08 13:26:03 +0900

    add siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,8 @@ module.exports = {
   siteMetadata: {
     siteUrl: "https://www.shgnkn.io",
     title: "blog.shgnkn.io",
+    description:
+      "This blog is for recording and disseminating what I have learned and tried.",
   },
   plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
 };
