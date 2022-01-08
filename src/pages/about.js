bc83e3ce7b056772ae51e850535c685c1983b2fe
shogo-nakano-desu <61229807+shogo-nakano-desu@users.noreply.github.com>
2022-01-08 12:00:10 +0900

    use layout component

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+// Step 1: Import React
+import * as React from "react";
+import Layout from "../components/layout";
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p>
+        Hi there! I'm the proud creator of this site, which I built with Gatsby.
+      </p>
+    </Layout>
+  );
+};
+// Step 3: Export your component
+export default AboutPage;
