b2bc9c87b2617cb0f48344a50fa41dda4ada1322
shogo-nakano-desu <61229807+shogo-nakano-desu@users.noreply.github.com>
2022-01-24 18:46:05 +0900

    comment out unnecessaries

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,20 +79,20 @@ module.exports = {
         ],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
-        gtagConfig: {
-          optimize_id: "OPT_CONTAINER_ID",
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
+        // gtagConfig: {
+        //   optimize_id: "OPT_CONTAINER_ID",
+        //   anonymize_ip: true,
+        //   cookie_expires: 0,
+        // },
         // This object is used for configuration specific to this plugin
-        pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: false,
-          // Setting this parameter is also optional
-          respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          exclude: ["/preview/**", "/do-not-track/me/too/"],
-        },
+        // pluginConfig: {
+        // Puts tracking script in the head instead of the body
+        // head: false,
+        // Setting this parameter is also optional
+        // respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        // exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // },
       },
     },
   ],
