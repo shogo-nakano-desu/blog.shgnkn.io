430876600c53363e0f78ecdf73d214a5a6403d9a
shogo-nakano-desu <61229807+shogo-nakano-desu@users.noreply.github.com>
2022-01-26 23:07:32 +0900

    change sh

--- sync.sh
+++ sync.sh
@@ -1,1 +1,9 @@
-rsync -azr --delete -e "ssh -i $secret_key -p $server_port" ./public/ $user_name@$server_port:$server_destination
+set -eu
+
+SSHPATH="$HOME/.ssh"
+if [ ! -d "$SSHPATH" ]; then
+  mkdir -p "$SSHPATH"
+fi
+echo "$secret_key" > "$SSHPATH/key"
+chmod 600 "$SSHPATH/key"
+rsync -azr --delete -e 'ssh -i $SSHPATH/key -p $server_port' ./public/ $user_name@$server_port:$server_destination
