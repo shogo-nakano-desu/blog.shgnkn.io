63546f70fff6b16188b56437377d62539b778749
shogo-nakano-desu <61229807+shogo-nakano-desu@users.noreply.github.com>
2022-02-15 09:50:17 +0900

    add rss icon

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { useStaticQuery, graphql, Link } from "gatsby";
 import { StaticImage } from 'gatsby-plugin-image'
 import * as styles from "./header.module.css";
+import { startOfYesterday } from 'date-fns';
 
 
 export const Header: React.FC= () => {
@@ -26,7 +27,10 @@ export const Header: React.FC= () => {
             <StaticImage className={styles.image} alt="github octcat icon" src="../images/GitHub-Mark-Light-32px.png" />
           </a>
           <a className={styles.icons} href="https://twitter.com/__shogo__">
-            <StaticImage className={styles.image} alt="twitter bird icon" src="../images/Twitter-white.png"></StaticImage>
+            <StaticImage className={styles.image} alt="twitter bird icon" src="../images/Twitter-white.png"/>
+          </a>
+          <a className={styles.icons} href="https://blog.shgnkn.io/rss.xml">
+            <StaticImage className={styles.image} alt="rss icon" src="../images/rss-white.png"/>
           </a>
           </div>
       </div>
