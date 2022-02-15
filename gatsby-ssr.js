1a2631a4e9b7de8121594c311cfab4fa64edf186
shogo-nakano-desu <61229807+shogo-nakano-desu@users.noreply.github.com>
2022-02-16 00:48:07 +0900

    try

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,21 +1,17 @@
-export const onPreRenderHTML = ({
+export const onPreRenderHTML = function onPreRenderHTML({
   getHeadComponents,
   replaceHeadComponents,
-}) => {
+}) {
   const headComponents = getHeadComponents();
   headComponents.sort((a, b) => {
-    if (a.type === b.type || (a.type !== "style" && b.type !== "style")) {
-      return 0;
-    }
-
-    if (a.type === "style") {
-      return 1;
-    } else if (b.type === "style") {
+    if (a.type === "meta") {
       return -1;
+    } else if (b.type === "meta") {
+      return 1;
     }
-
     return 0;
   });
 
   replaceHeadComponents(headComponents);
+  console.log(getHeadComponents());
 };
